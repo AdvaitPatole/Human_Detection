df85101facf9c68c852d43df286ea25b0fd288df
AdvaitPatole <93151467+AdvaitPatole@users.noreply.github.com>
2021-10-25 19:11:01 -0400

    Update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -1,14 +1,5 @@
-sudo apt-get install build-essential checkinstall cmake pkg-config yasm gfortran git
-sudo apt-get install libjpeg8-dev libjasper-dev libpng12-dev
-sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev
-sudo apt-get install libxine2-dev libv4l-dev
-sudo apt-get install libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev
-sudo apt-get install libqt4-dev libgtk2.0-dev libtbb-dev
-sudo apt-get install libatlas-base-dev
-sudo apt-get install libfaac-dev libmp3lame-dev libtheora-dev
-sudo apt-get install libvorbis-dev libxvidcore-dev
-sudo apt-get install libopencore-amrnb-dev libopencore-amrwb-dev
-sudo apt-get install x264 v4l-utils
+### sudo apt-get install build-essential checkinstall cmake pkg-config yasm gfortran git
+
 
 ### For opencv
 
