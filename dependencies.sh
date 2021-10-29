4516a8c4cf6af1bc4121b2e43c9f75d54914b243
AdvaitPatole <93151467+AdvaitPatole@users.noreply.github.com>
2021-10-29 02:03:01 -0400

    Update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -1,25 +1,42 @@
-git clone https://github.com/opencv/opencv.git
-cd opencv 
-git checkout 4.5.0
-cd ..
-git clone https://github.com/opencv/opencv_contrib.git
-cd opencv_contrib
-git checkout 4.5.0
-cd ..
-cd opencv
+# Install Eigen
+sudo apt install libeigen3-dev
+sudo apt-get install -qq g++-7
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-7 90
+
+# Install lcov for code coverage
+sudo apt-get install -y -qq lcov
+
+# OpenCV dependencies
+sudo apt-get install -y build-essential
+sudo apt-get install -y cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
+
+sudo apt-get install -y python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
+
+wget -O opencv.zip https://github.com/opencv/opencv/archive/4.5.1.zip
+wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/4.5.1.zip
+unzip opencv.zip
+unzip opencv_contrib.zip
+
+cd opencv-4.5.1
 mkdir build
 cd build
+
 cmake -D CMAKE_BUILD_TYPE=RELEASE \
       -D BUILD_EXAMPLES=OFF \
       -D BUILD_opencv_apps=OFF \
-      -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules \
       -D BUILD_DOCS=OFF \
       -D BUILD_PERF_TESTS=OFF \
       -D BUILD_TESTS=OFF \
-      -D WITH_CUDA=OFF \
+      -D OPENCV_ENABLE_NONFREE=ON \
+      -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib-4.5.1/modules \
       -D CMAKE_INSTALL_PREFIX=/usr/local ..
-      
-make 
+
+make -j$(nproc)
+
 sudo make install
-sudo sh -c 'echo "/usr/local/lib" >> /etc/ld.so.conf.d/opencv.conf'
+
+sudo sh -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf'
 sudo ldconfig
+cd ../../
+sudo rm -rf opencv-4.5.1
+sudo rm -rf opencv_contrib-4.5.1
